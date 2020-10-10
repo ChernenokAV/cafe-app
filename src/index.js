17fdf4451d9a8f5226ecd336127c84875ad26181
ChernenokAV <chernenok.alexandr@yandex.by>
2020-10-10 16:49:47 +0300

    First commit cafe-app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+function Index() {
+  return <App />;
+}
+
+ReactDom.render(<Index />, document.getElementById("root"));
